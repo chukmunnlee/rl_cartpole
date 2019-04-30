31c314642e8e44aab063c722a515a4e645165693
Chuk <chukmunnlee@gmail.com>
2019-04-30 11:47:45 +0800

    implemented policy gradient

--- pg.py
+++ pg.py
@@ -18,19 +18,22 @@ def policy(st, th):
    prob = softmax(st, th)
    return np.random.choice(len(prob), p=prob)
 
-def value(st, w, a=None):
-   result = np.dot(w, st)
-   return result if a is None else result[a]
+def value(st, w):
+   return np.dot(w, st)
 
 #env = gym.make('CartPole-v1')
 env = gym.make('CartPole-v0')
 
-th = np.ones(shape=(2, 4))
-w = np.ones(shape=(2, 4))
+#th = np.ones(shape=(2, 4))
+th = np.random.rand(2, 4)
+w = np.ones(shape=(1, 4))
+
+print('th = ', th)
 
 total_reward = []
 
-for i in tqdm(range(EPISODES)):
+#for i in tqdm(range(EPISODES)):
+for i in range(EPISODES):
    state = env.reset()
    done = False
    cum_reward = 0
@@ -43,20 +46,37 @@ for i in tqdm(range(EPISODES)):
       cum_reward += reward
 
       if done:
+         #print('Episode: %d, reward: %d' %(i, cum_reward))
          td_target = reward
+         total_reward.append(cum_reward)
+
       else:
          td_target = reward + (GAMMA * value(new_state, w))
+
       td_error = td_target - value(state, w)
 
       #update critic
-      w[action] += ALPHA_CRITIC * td_error * state
+      w += ALPHA_CRITIC * td_error * state
 
       #update policy
       prob = softmax(state, th)
+#      print('prob = ', prob)
+#      print('\tth = ', th)
+#      print('\taction = ', action)
+#      print('\tth[action] = ', th[action])
+#      print('\tnp.dot = ', np.dot(th[action], state))
+#      print('\texpectation = ', sum([ prob[p] * np.dot(th[p], state) for p in range(len(prob))] ))
 
-      print('sum = ', sum([ prob[i] * np.dot(th[i], state) for i in range(len(prob)) ]))
-      print('log gradient = ', value(state, action) - sum([ prob[i] * np.dot(th[i], state) for i in range(len(prob))]))
-      """
-      th[action] += ALPHA_GP * GAMMA * 
-            (value(state, action) - sum([ prob[i] * np.dot(th[i], state) for i in range(len(prob))]))
-      """
+      th[action] += ALPHA_GP * GAMMA * (
+            np.dot(th[action], state) - sum([ prob[p] * np.dot(th[p], state) for p in range(len(prob))] ))
+
+      state = new_state
+
+env.close()
+
+print('Average reward: %.3f' %(sum(total_reward) / len(total_reward)))
+
+plt.plot(range(len(total_reward)), total_reward, label='EPISODES %d' %EPISODES)
+plt.legend()
+
+plt.show()
